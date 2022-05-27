11790208b2c72fd7bbfa6b9b54d4bb3423a44801
pkdch <pau.darco371@gmail.com>
2022-05-27 15:13:20 +0100

    check if autocert fills in the server name automatically5

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 func MyGetCertificate(man *autocert.Manager) func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
 	return func(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
-		// hello.ServerName = "www.elephorum.com"
+		hello.ServerName = "www.elephorum.com"
 		fmt.Printf("https ClientHelloInfo's ServerName: %s\n", hello.ServerName)
 		// cipher suite
 		return man.GetCertificate(hello)
